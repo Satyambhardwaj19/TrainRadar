7aaf1de96e9457ef632c2a924b12d09873f35ded
Satyambhardwaj19 <satyambhardwaj911@gmail.com>
2026-06-29 18:33:45 +0100

    Add HTML templates and Leaflet.js map of UK

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Welcome to TrainRadar</h1><p>Live UK train tracking coming soon.</p>'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About TrainRadar</h1><p>This app was built by Satyam Bhardwaj as part of an MSc dissertation at the University of Liverpool.</p>'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
